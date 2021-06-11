bb09ff0b3e36e8e6c067a60082c29c1022c9a1ce
AxelNava <napa20020314@gmail.com>
2021-06-11 10:58:58 -0500

    Fracciones

--- juego/VentanaJuego.Designer.cs
+++ juego/VentanaJuego.Designer.cs
@@ -69,6 +69,20 @@ namespace juego
             this.player = new System.Windows.Forms.PictureBox();
             this.panel2Problemas = new System.Windows.Forms.Panel();
             this.MuestraProblemas = new System.Windows.Forms.Label();
+            this.labelSegundooperador = new System.Windows.Forms.Label();
+            this.labelPrimeroperador = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.DenPan3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.NumPan3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.NumPan1 = new System.Windows.Forms.Label();
+            this.DenPan1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.DenPan2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NumPan2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Musica)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.OverM)).BeginInit();
@@ -85,6 +99,9 @@ namespace juego
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.panel2Problemas.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -422,9 +439,9 @@ namespace juego
             // enemi
             // 
             this.enemi.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.enemi.Location = new System.Drawing.Point(708, 346);
+            this.enemi.Location = new System.Drawing.Point(708, 288);
             this.enemi.Name = "enemi";
-            this.enemi.Size = new System.Drawing.Size(134, 132);
+            this.enemi.Size = new System.Drawing.Size(186, 190);
             this.enemi.TabIndex = 49;
             this.enemi.TabStop = false;
             // 
@@ -495,30 +512,167 @@ namespace juego
             // player
             // 
             this.player.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.player.Location = new System.Drawing.Point(154, 346);
+            this.player.Location = new System.Drawing.Point(114, 288);
             this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(146, 132);
+            this.player.Size = new System.Drawing.Size(186, 190);
             this.player.TabIndex = 50;
             this.player.TabStop = false;
             // 
             // panel2Problemas
             // 
             this.panel2Problemas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2Problemas.Controls.Add(this.labelSegundooperador);
+            this.panel2Problemas.Controls.Add(this.labelPrimeroperador);
+            this.panel2Problemas.Controls.Add(this.panel5);
             this.panel2Problemas.Controls.Add(this.MuestraProblemas);
-            this.panel2Problemas.Location = new System.Drawing.Point(323, 203);
+            this.panel2Problemas.Controls.Add(this.panel4);
+            this.panel2Problemas.Controls.Add(this.panel3);
+            this.panel2Problemas.Location = new System.Drawing.Point(289, 100);
             this.panel2Problemas.Name = "panel2Problemas";
-            this.panel2Problemas.Size = new System.Drawing.Size(349, 132);
+            this.panel2Problemas.Size = new System.Drawing.Size(473, 182);
             this.panel2Problemas.TabIndex = 52;
             // 
             // MuestraProblemas
             // 
             this.MuestraProblemas.AutoSize = true;
             this.MuestraProblemas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MuestraProblemas.Location = new System.Drawing.Point(149, 60);
+            this.MuestraProblemas.Location = new System.Drawing.Point(381, 77);
             this.MuestraProblemas.Name = "MuestraProblemas";
             this.MuestraProblemas.Size = new System.Drawing.Size(60, 24);
             this.MuestraProblemas.TabIndex = 0;
             this.MuestraProblemas.Text = "label2";
+            // 
+            // labelSegundooperador
+            // 
+            this.labelSegundooperador.AutoSize = true;
+            this.labelSegundooperador.Location = new System.Drawing.Point(240, 83);
+            this.labelSegundooperador.Name = "labelSegundooperador";
+            this.labelSegundooperador.Size = new System.Drawing.Size(10, 13);
+            this.labelSegundooperador.TabIndex = 18;
+            this.labelSegundooperador.Text = "-";
+            // 
+            // labelPrimeroperador
+            // 
+            this.labelPrimeroperador.AutoSize = true;
+            this.labelPrimeroperador.Location = new System.Drawing.Point(120, 83);
+            this.labelPrimeroperador.Name = "labelPrimeroperador";
+            this.labelPrimeroperador.Size = new System.Drawing.Size(13, 13);
+            this.labelPrimeroperador.TabIndex = 17;
+            this.labelPrimeroperador.Text = "+";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.DenPan3);
+            this.panel5.Controls.Add(this.label8);
+            this.panel5.Controls.Add(this.NumPan3);
+            this.panel5.Location = new System.Drawing.Point(268, 33);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(73, 105);
+            this.panel5.TabIndex = 16;
+            this.panel5.Visible = false;
+            // 
+            // DenPan3
+            // 
+            this.DenPan3.AutoSize = true;
+            this.DenPan3.Location = new System.Drawing.Point(17, 75);
+            this.DenPan3.Name = "DenPan3";
+            this.DenPan3.Size = new System.Drawing.Size(35, 13);
+            this.DenPan3.TabIndex = 8;
+            this.DenPan3.Text = "label9";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "________";
+            // 
+            // NumPan3
+            // 
+            this.NumPan3.AutoSize = true;
+            this.NumPan3.Location = new System.Drawing.Point(17, 17);
+            this.NumPan3.Name = "NumPan3";
+            this.NumPan3.Size = new System.Drawing.Size(35, 13);
+            this.NumPan3.TabIndex = 6;
+            this.NumPan3.Text = "label7";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.NumPan1);
+            this.panel4.Controls.Add(this.DenPan1);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(31, 33);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(73, 105);
+            this.panel4.TabIndex = 15;
+            this.panel4.Visible = false;
+            // 
+            // NumPan1
+            // 
+            this.NumPan1.AutoSize = true;
+            this.NumPan1.Location = new System.Drawing.Point(10, 19);
+            this.NumPan1.Name = "NumPan1";
+            this.NumPan1.Size = new System.Drawing.Size(54, 13);
+            this.NumPan1.TabIndex = 3;
+            this.NumPan1.Text = "NumPan1";
+            // 
+            // DenPan1
+            // 
+            this.DenPan1.AutoSize = true;
+            this.DenPan1.Location = new System.Drawing.Point(10, 74);
+            this.DenPan1.Name = "DenPan1";
+            this.DenPan1.Size = new System.Drawing.Size(52, 13);
+            this.DenPan1.TabIndex = 2;
+            this.DenPan1.Text = "DenPan1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "________";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.DenPan2);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.NumPan2);
+            this.panel3.Location = new System.Drawing.Point(150, 33);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(73, 105);
+            this.panel3.TabIndex = 14;
+            this.panel3.Visible = false;
+            // 
+            // DenPan2
+            // 
+            this.DenPan2.AutoSize = true;
+            this.DenPan2.Location = new System.Drawing.Point(19, 72);
+            this.DenPan2.Name = "DenPan2";
+            this.DenPan2.Size = new System.Drawing.Size(35, 13);
+            this.DenPan2.TabIndex = 5;
+            this.DenPan2.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "________";
+            // 
+            // NumPan2
+            // 
+            this.NumPan2.AutoSize = true;
+            this.NumPan2.Location = new System.Drawing.Point(19, 20);
+            this.NumPan2.Name = "NumPan2";
+            this.NumPan2.Size = new System.Drawing.Size(35, 13);
+            this.NumPan2.TabIndex = 3;
+            this.NumPan2.Text = "label4";
             // 
             // VentanaJuego
             // 
@@ -552,6 +706,12 @@ namespace juego
             ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             this.panel2Problemas.ResumeLayout(false);
             this.panel2Problemas.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -597,5 +757,19 @@ namespace juego
         private System.Windows.Forms.Button tres;
         private System.Windows.Forms.Button dos;
         private System.Windows.Forms.Button uno;
+        private System.Windows.Forms.Label labelSegundooperador;
+        private System.Windows.Forms.Label labelPrimeroperador;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label DenPan3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label NumPan3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label NumPan1;
+        private System.Windows.Forms.Label DenPan1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label DenPan2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label NumPan2;
     }
 }
